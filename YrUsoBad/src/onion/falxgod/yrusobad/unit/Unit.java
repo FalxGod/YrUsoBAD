7d3843901484fa9745f4ae58ea323af3a3baae10
BaronNox <baron.von.stabossem@gmail.com>
2018-05-18 02:00:53 +0200

    +init unit

---
+++ YrUsoBad/src/onion/falxgod/yrusobad/unit/Unit.java
@@ -0,0 +1,11 @@
+package onion.falxgod.yrusobad.unit;
+
+import onion.falxgod.yrusobad.references.UnitTypes;
+
+public class Unit {
+	private String name;
+	private int tier;
+	private UnitTypes type;
+	
+	
+}
