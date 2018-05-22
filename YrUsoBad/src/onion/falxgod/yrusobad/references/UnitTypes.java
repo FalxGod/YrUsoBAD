85fccca0c77861ad190f11c72f5a5a464ba58f51
BaronNox <baron.von.stabossem@gmail.com>
2018-05-22 20:56:55 +0200

    +added utility methods

--- YrUsoBad/src/onion/falxgod/yrusobad/references/UnitTypes.java
+++ YrUsoBad/src/onion/falxgod/yrusobad/references/UnitTypes.java
@@ -1,10 +1,36 @@
 package onion.falxgod.yrusobad.references;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public enum UnitTypes {
 	FALX, SWORD, SPEAR, SWORD_CAV, SPEAR_CAV, ARCHER, SLINGER, JAVELIN, HEAVY_ARTY, SLIGHT_ARTY, ELEPHANT;
 	
 	@Override
 	public String toString() {
 		return name().toLowerCase();
+	}
+	
+	public static boolean contains(UnitTypes unitType) {
+		if(unitType != null && Arrays.asList(UnitTypes.values()).contains(unitType)) return true;
+		
+		return false;
+	}
+	public static boolean contains(String unitType) {
+		if(unitType != null) {
+			for(UnitTypes uT : Arrays.asList(UnitTypes.values())) {
+				if(uT.toString().equals(unitType)) return true;
+			}
+		}
+		return false;
+	}
+	
+	public static UnitTypes getFromSring(String unitType) {
+		if(unitType != null) {
+			for(UnitTypes uT : Arrays.asList(UnitTypes.values())) {
+				if(uT.toString().equals(unitType)) return uT;
+			}
+		}
+		return null;
 	}
 }
